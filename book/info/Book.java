05a222a31e7e88b2d2c266939e562388059377da
Lương Minh Triết <72116161+minhtriet2906@users.noreply.github.com>
2021-04-23 11:12:25 +0700

    Add files via upload

--- book/info/Book.java
+++ book/info/Book.java
@@ -1,32 +1,48 @@
 package book.info;
-public class Book {
 
-	public Book(String title, int author, String category, String publisher) {
+public class Book {
+	private String title;
+	
+	private String author;
+	
+	private String category;
+	
+	private String publisher;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	
+	
+	public Book(String title, String author, String category, String publisher) {
 		super();
 		this.title = title;
 		this.author = author;
 		this.category = category;
 		this.publisher = publisher;
 	}
-
-	public Book(String title, int author) {
-		super();
-		this.title = title;
-		this.author = author;
-	}
-
-	public Book(String title, int author, String category) {
-		super();
-		this.title = title;
-		this.author = author;
-		this.category = category;
-	}
-	private String title;
-
-	private int author;
-
-	private String category;
-
-	private String publisher;
 	
+	
+
 }
