96b9075c738a9bb73365adde882e7f15ae892d27
ninh-nd <ninh.nd194815@sis.hust.edu.vn>
2021-04-23 10:26:20 +0700

    Adding constructors

--- book/info/Book.java
+++ book/info/Book.java
@@ -1,6 +1,26 @@
 package book.info;
 public class Book {
 
+	public Book(String title, int author, String category, String publisher) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.category = category;
+		this.publisher = publisher;
+	}
+
+	public Book(String title, int author) {
+		super();
+		this.title = title;
+		this.author = author;
+	}
+
+	public Book(String title, int author, String category) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.category = category;
+	}
 	private String title;
 
 	private int author;
@@ -8,5 +28,5 @@ public class Book {
 	private String category;
 
 	private String publisher;
-
+	
 }
