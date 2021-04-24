5faa00f713e7d234ddefb495142bb47007d550aa
ninh-nd <ninh.nd194815@sis.hust.edu.vn>
2021-04-24 20:10:04 +0700

    Add placeholder for main menu

---
+++ menu/MainMenu.java
@@ -0,0 +1,14 @@
+package menu;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+public class MainMenu {
+	public MainMenu() {
+
+	}
+
+	public static void main(String[] args) {
+	}
+
+}
