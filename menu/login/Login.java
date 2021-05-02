28d033e27cdac62d6b9ae86fc8018c510100f06e
ninh-nd <ninh.nd194815@sis.hust.edu.vn>
2021-05-02 20:47:49 +0700

    Modified login against SQL injection

--- menu/login/Login.java
+++ menu/login/Login.java
@@ -14,49 +14,40 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+
+import menu.DatabaseManagement;
+
 import java.sql.*;
 
 public class Login {
-	private static final String url = "jdbc:postgresql://localhost/Library";
-	private static final String user = "postgres";
-	private static final String password = "123456";
 	private JFrame frame;
 	private JTextField textField;
 	private JPasswordField passwordField;
 	private JButton exitButton;
 	private JLabel loginMenuLabel;
-	/**
-	 * Establish connection to the database
-	 */
-	public Connection connect() {
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(url, user, password);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-
-        return conn;
-    }
+	private DatabaseManagement db = new DatabaseManagement();
 	/**
 	 * Check username and password from database
 	 */
 	private void checkLogin(String username, String password) {
-		String sql = "SELECT * from users WHERE username='" + username + "' and password='" + password + "'";
-		 try (Connection conn = connect();
-	                Statement stmt = conn.createStatement();
-	                ResultSet rs = stmt.executeQuery(sql)) {
-		        if (username != null && password != null) {
-		            if (rs.next()) {
-		               JOptionPane.showMessageDialog(null, "Login Successfully");
-		            } else {
-		            	JOptionPane.showMessageDialog(null, "Wrong username/password", "Login Error", JOptionPane.ERROR_MESSAGE);
-		            }
-		        }
-		    } catch (SQLException err) {
-		    	 System.out.println(err.getMessage());
-		    }
-	}
+        String sql = "SELECT * from users WHERE username= ? and password= ? ";
+        try  {
+            Connection conn = db.connect();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1,username);
+            stmt.setString(2,password);
+            ResultSet rs = stmt.executeQuery();
+            if (username != null && password != null) {
+                if (rs.next()) {
+                    JOptionPane.showMessageDialog(null, "Login Successfully");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Wrong username/password", "Login Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        } catch (SQLException err) {
+            System.out.println(err.getMessage());
+        }
+    }
 	/**
 	 * Launch the application.
 	 */
@@ -65,7 +56,7 @@ public class Login {
 			public void run() {
 				try {
 					Login window = new Login();
-					window.connect();
+					window.db.connect();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
