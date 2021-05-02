e032e28689383b8486e49de953f6266c6a37c4b3
Lương Minh Triết <72116161+minhtriet2906@users.noreply.github.com>
2021-05-02 11:22:45 +0700

    Add files via upload

--- system/management/Account.java
+++ system/management/Account.java
@@ -1,26 +1,65 @@
-package system.management;
+package management;
 import java.util.*;
 
-import system.info.Person;
+import info.Person;
 
-public class Account {
 
-	private String id;
+public class Account extends Person{
+	public Account(String name, String email, String phoneNumber, String address, String username, String password) {
+		super(name, email, phoneNumber, address);
+		this.username = username;
+		this.password = password;
+		// TODO Auto-generated constructor stub
+	}
+	List<Account> Account = new ArrayList<Account>();
+	Scanner sc = new Scanner(System.in);
+
+	private String username;
 
 	private String password;
 
 	private Person info;
-
+	
 	public void logIn() {
-
+	
 	}
 
 	public void logOut() {
 
 	}
-
+	
+	public void addAcc(Account account) {
+		if(Account.contains(account))
+			return ;
+		else 
+			Account.add(account);		
+	}
 	public void createNewAccount() {
-
+		
+		
+		System.out.println("Create new account");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		
+		System.out.print("Email: ");
+		String email = sc.nextLine();
+		
+		System.out.print("Phonenumber: ");
+		String phonenumber = sc.nextLine();
+		
+		System.out.print("Address: ");
+		String address = sc.nextLine();
+		
+		System.out.print("Create Username ");
+		String username = sc.nextLine();
+		
+		System.out.print("Create Password: ");
+		String password = sc.nextLine();
+		
+		Account account = new Account(name, email, phonenumber, address, username, password);
+		Account.add(account);
+		
+		System.out.println("Success ");
 	}
 
 }
