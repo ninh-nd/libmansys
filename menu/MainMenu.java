74cd28d255837f5d626019ccc1f7109ad4fafd64
ninh-nd <ninh.nd194815@sis.hust.edu.vn>
2021-05-02 14:39:08 +0700

    Add main menu GUI (no function yet)

--- menu/MainMenu.java
+++ menu/MainMenu.java
@@ -1,14 +1,79 @@
 package menu;
-import java.awt.event.ActionEvent;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-import javax.swing.*;
 public class MainMenu {
-	public MainMenu() {
 
-	}
+	private JFrame frame;
 
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainMenu window = new MainMenu();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
+	/**
+	 * Create the application.
+	 */
+	public MainMenu() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 452, 553);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel title = new JLabel("Library Management System");
+		title.setBounds(0, 66, 436, 29);
+		title.setHorizontalAlignment(SwingConstants.CENTER);
+		title.setFont(new Font("Tahoma", Font.BOLD, 24));
+		frame.getContentPane().add(title);
+		
+		JButton loginButton = new JButton("Login");
+		loginButton.setBounds(142, 180, 152, 47);
+		frame.getContentPane().add(loginButton);
+		
+		JButton newAccountButton = new JButton("Create new account");
+		newAccountButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		newAccountButton.setBounds(142, 270, 152, 47);
+		frame.getContentPane().add(newAccountButton);
+		
+		JButton exitButton = new JButton("Exit");
+		exitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		exitButton.setBounds(142, 360, 152, 47);
+		frame.getContentPane().add(exitButton);
+	}
 }
