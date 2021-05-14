328f91bb0c40c3b89af478e017d17e90fbd6862a
ninh-nd <ninh.nd194815@sis.hust.edu.vn>
2021-05-14 15:15:22 +0700

    Add view book option

---
+++ usermenu/ViewBook.java
@@ -0,0 +1,43 @@
+package usermenu;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class ViewBook {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ViewBook window = new ViewBook();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ViewBook() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 738, 427);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
