b875c357c94b69faba108e654b115b30275009c9
ninh-nd <ninh.nd194815@sis.hust.edu.vn>
2021-05-17 16:34:22 +0700

    Rewrite view book function for better reusability

--- usermenu/ViewBook.java
+++ usermenu/ViewBook.java
@@ -1,4 +1,5 @@
 package usermenu;
+
 import net.proteanit.sql.DbUtils;
 import java.awt.EventQueue;
 import java.sql.*;
@@ -30,6 +31,7 @@ public class ViewBook {
 	private static DatabaseManagement db = new DatabaseManagement();
 	private JTable table = new JTable();
 	private JTextField searchField;
+
 	/**
 	 * Launch the application.
 	 */
@@ -45,21 +47,23 @@ public class ViewBook {
 			}
 		});
 	}
+
 	/**
 	 * Create the application.
 	 */
 	public ViewBook() {
 		String sql = "SELECT * FROM books";
-		try  {
-            Connection conn = db.connect();
-            PreparedStatement stmt = conn.prepareStatement(sql); 
-            ResultSet rs = stmt.executeQuery();
-            table.setModel(DbUtils.resultSetToTableModel(rs));
+		try {
+			Connection conn = db.connect();
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+			table.setModel(DbUtils.resultSetToTableModel(rs));
 		} catch (SQLException err) {
-            System.out.println(err.getMessage());
+			System.out.println(err.getMessage());
 		}
 		initialize();
 	}
+
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -67,68 +71,70 @@ public class ViewBook {
 		frmBookList = new JFrame();
 		frmBookList.setTitle("Book list");
 		frmBookList.setVisible(true);
-		frmBookList.setBounds(100, 100, 629, 377);
+		frmBookList.setBounds(100, 100, 788, 377);
 		frmBookList.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmBookList.getContentPane().setLayout(null);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(0, 145, 611, 193);
+		scrollPane.setBounds(0, 145, 772, 193);
 		frmBookList.getContentPane().add(scrollPane);
 		scrollPane.setViewportView(table);
-		
+
 		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 611, 144);
+		panel.setBounds(0, 0, 772, 144);
 		frmBookList.getContentPane().add(panel);
 		panel.setLayout(null);
-		
+
 		searchField = new JTextField();
-		searchField.setBounds(239, 47, 133, 20);
+		searchField.setBounds(276, 47, 219, 20);
 		panel.add(searchField);
 		searchField.setColumns(10);
-		
+
 		JPanel optionPanel = new JPanel();
-		optionPanel.setBounds(0, 5, 611, 36);
+		optionPanel.setBounds(0, 5, 772, 36);
 		panel.add(optionPanel);
 		optionPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
+
 		JRadioButton searchTitle = new JRadioButton("Search by title");
 		optionPanel.add(searchTitle);
-		
-		
+
 		JRadioButton searchCategory = new JRadioButton("Search by category");
 		optionPanel.add(searchCategory);
-		
+
 		JRadioButton searchAuthor = new JRadioButton("Search by author");
 		optionPanel.add(searchAuthor);
-		
-		
+
 		JRadioButton searchPublisher = new JRadioButton("Search by publisher");
 		optionPanel.add(searchPublisher);
-		
-		ButtonGroup group = new ButtonGroup(); //Add buttons into a group so that one can only choose one radiobox 
+
+		ButtonGroup group = new ButtonGroup(); // Add buttons into a group so that one can only choose one radiobox
 		group.add(searchTitle);
 		group.add(searchPublisher);
 		group.add(searchCategory);
 		group.add(searchAuthor);
-		
+
 		JButton searchButton = new JButton("Search");
-		frmBookList.getRootPane().setDefaultButton(searchButton); //Pressing Enter will also search
+		frmBookList.getRootPane().setDefaultButton(searchButton); // Pressing Enter will also search
 		searchButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (searchCategory.isSelected()) Search.searchCategory(searchField.getText(), table);
-				
-				if (searchTitle.isSelected()) Search.searchTitle(searchField.getText(), table);
-				
-				if (searchPublisher.isSelected()) Search.searchPublisher(searchField.getText(), table);
-				
-				if (searchAuthor.isSelected()) Search.searchAuthor(searchField.getText(), table);
-				
-				if (searchField.getText().isBlank()) Search.showAllBook(table); //Return list of all book if the search field is empty
+				if (searchCategory.isSelected() && !searchField.getText().isBlank())
+					table.setModel(DbUtils.resultSetToTableModel(Search.searchCategory(searchField.getText())));
+
+				if (searchTitle.isSelected() && !searchField.getText().isBlank())
+					table.setModel(DbUtils.resultSetToTableModel(Search.searchTitle(searchField.getText())));
+
+				if (searchPublisher.isSelected() && !searchField.getText().isBlank())
+					table.setModel(DbUtils.resultSetToTableModel(Search.searchPublisher(searchField.getText())));
+
+				if (searchAuthor.isSelected() && !searchField.getText().isBlank())
+					table.setModel(DbUtils.resultSetToTableModel(Search.searchAuthor(searchField.getText())));
+
+				if (searchField.getText().isBlank())
+					table.setModel(DbUtils.resultSetToTableModel(Search.showAllBook())); // Return list of all book if
+																							// the search field is empty
 			}
 		});
-		searchButton.setBounds(261, 93, 89, 23);
+		searchButton.setBounds(341, 78, 89, 23);
 		panel.add(searchButton);
-		
-		
 	}
 }
--- utilities/Search.java
+++ utilities/Search.java
@@ -1,96 +1,97 @@
 package utilities;
+
 import java.sql.*;
 
 import javax.swing.JTable;
 
 import menu.DatabaseManagement;
 import net.proteanit.sql.DbUtils;
+
 public class Search {
 	private static DatabaseManagement db = new DatabaseManagement();
-	public static void searchTitle(String title, JTable table) {
-		if(!title.trim().isEmpty()) {
-			try(Connection conn	= db.connect();){
-				//String process: Convert the search string to all lowercase
+
+	public static ResultSet searchTitle(String title) {
+		if (!title.trim().isEmpty()) {
+			try (Connection conn = db.connect();) {
+				// String process: Convert the search string to all lowercase
 				title = title.toLowerCase();
 				String sql = "SELECT * FROM books WHERE LOWER(title) LIKE ?";
 				PreparedStatement stmt = conn.prepareStatement(sql);
 				stmt.setString(1, "%" + title + "%");
 				ResultSet rs = stmt.executeQuery();
-				table.setModel(DbUtils.resultSetToTableModel(rs));
-				stmt.close();
-				
-			}catch (SQLException err) {
+				return rs;
+			} catch (SQLException err) {
 				System.out.println(err.getMessage());
 			}
 		}
+		return null;
 	}
-	public static void searchCategory(String category, JTable table) {
-		if(!category.trim().isEmpty()) {
-			try(Connection conn	= db.connect();){
-				//String process: Convert the search string to all lowercase
+
+	public static ResultSet searchCategory(String category) {
+		if (!category.trim().isEmpty()) {
+			try (Connection conn = db.connect();) {
+				// String process: Convert the search string to all lowercase
 				category = category.toLowerCase();
 				String sql = "SELECT * FROM books WHERE LOWER(category) LIKE ?";
 				PreparedStatement stmt = conn.prepareStatement(sql);
 				stmt.setString(1, "%" + category + "%");
 				ResultSet rs = stmt.executeQuery();
-				table.setModel(DbUtils.resultSetToTableModel(rs));
-				stmt.close();
-				
-			}catch (SQLException err) {
+				return rs;
+			} catch (SQLException err) {
 				System.out.println(err.getMessage());
 			}
 		}
+		return null;
 	}
-	public static void searchPublisher(String publisher, JTable table) {
-		if(!publisher.trim().isEmpty()) {
-			try(Connection conn	= db.connect();){
-				//String process: Convert the search string to all lowercase
+
+	public static ResultSet searchPublisher(String publisher) {
+		if (!publisher.trim().isEmpty()) {
+			try (Connection conn = db.connect();) {
+				// String process: Convert the search string to all lowercase
 				publisher = publisher.toLowerCase();
 				String sql = "SELECT * FROM books WHERE LOWER(publisher) LIKE ?";
 				PreparedStatement stmt = conn.prepareStatement(sql);
 				stmt.setString(1, "%" + publisher + "%");
 				ResultSet rs = stmt.executeQuery();
-				table.setModel(DbUtils.resultSetToTableModel(rs));
-				stmt.close();
-				
-			}catch (SQLException err) {
+				return rs;
+			} catch (SQLException err) {
 				System.out.println(err.getMessage());
 			}
 		}
+		return null;
 	}
-	
-	public static void searchAuthor(String author, JTable table) {
-		if(!author.trim().isEmpty()) {
-			try(Connection conn	= db.connect();){
-				//String process: Convert the search string to all lowercase
+
+	public static ResultSet searchAuthor(String author) {
+		if (!author.trim().isEmpty()) {
+			try (Connection conn = db.connect();) {
+				// String process: Convert the search string to all lowercase
 				author = author.toLowerCase();
 				String sql = "SELECT * FROM books WHERE LOWER(author) LIKE ?";
 				PreparedStatement stmt = conn.prepareStatement(sql);
 				stmt.setString(1, "%" + author + "%");
 				ResultSet rs = stmt.executeQuery();
-				table.setModel(DbUtils.resultSetToTableModel(rs));
-				stmt.close();
-				
-			}catch (SQLException err) {
+				return rs;
+			} catch (SQLException err) {
 				System.out.println(err.getMessage());
 			}
 		}
+		return null;
 	}
 
-	public static void showAllBook(JTable table) {
-		try(Connection conn	= db.connect();){
+	public static ResultSet showAllBook() {
+		try (Connection conn = db.connect();) {
 			String sql = "SELECT * FROM books";
 			PreparedStatement stmt = conn.prepareStatement(sql);
 			ResultSet rs = stmt.executeQuery();
-			table.setModel(DbUtils.resultSetToTableModel(rs));
-			stmt.close();
-			
-		}catch (SQLException err) {
+			return rs;
+		} catch (SQLException err) {
 			System.out.println(err.getMessage());
 		}
+		return null;
 	}
+
 	public static void main(String[] args) {
-		
+
 	}
 
 }
